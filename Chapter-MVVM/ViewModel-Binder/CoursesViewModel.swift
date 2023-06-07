cc7b5ebeceefbc31f3e80376fee04dfd09b50976
Tori Lew <torirlew@gmail.com>
2023-06-06 22:41:56 -0700

    Adding course sections

--- Chapter-MVVM/ViewModel-Binder/CoursesViewModel.swift
+++ Chapter-MVVM/ViewModel-Binder/CoursesViewModel.swift
@@ -29,14 +29,51 @@ extension CoursesView {
                     }
                     print("Adding \(courseName) to \(discName)")
                     // add new course to existing discipline:
-                    let course = Course(name: courseName)
+                    let course = Course(name: courseName, sections: [])
                     theCollege.allClasses[idx].courses.append(course)
                     return
                 }
             }
             print("Couldn't find discipline named \(discName), so create an entirely new one")
-            let course = Discipline(name: discName, courses: [Course(name: courseName)])
+            let course = Discipline(name: discName, courses: [Course(name: courseName, sections: [])])
             allClasses.append(course)
+            return
+        }
+        func addNewSection( time: String, courseName: String, discName: String) {
+            print("Create new section: \(courseName) \(time)")
+            
+            for idx in theCollege.allClasses.indices {
+                for index in theCollege.allClasses[idx].courses.indices{
+                    print("\tChecking \(theCollege.allClasses[idx].courses[index])")
+                    
+                    if theCollege.allClasses[idx].courses[index].name == courseName {
+                        print("\tFound course named \(courseName)")
+                        for section in theCollege.allClasses[idx].courses[index].sections {
+                            if section.time == time {
+                                print("\tFound duplicate - ignoring")
+                                return
+                            }
+                        }
+                        print("Adding \(time) to \(courseName)")
+                        // add new course to existing discipline:
+                        let section = Sect(time: time)
+                        theCollege.allClasses[idx].courses[index].sections.append(section)
+                        return
+                    }
+                }
+                
+                print("Couldn't find course named \(courseName), so create an entirely new one")
+                addNewCourse(discName: discName, courseName: courseName)
+                return
+            }
+        }
+        
+        func enrollSection(course: Course, disc: Discipline) {
+            for var sect in theCollege.allClasses[disc.id.hashValue].courses[course.id.hashValue].sections {
+                if sect.isEnrolled == true{
+                    sect.isEnrolled.toggle()
+                }
+            }
             return
         }
     }
